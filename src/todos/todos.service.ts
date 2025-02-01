a874e7b7245e275976b7779ab3bea80f3026a83f
Jomar <kuyaj27@gmail.com>
2025-02-01 20:25:33 +0800

    [Jomar][refractor/services] optimizing codes in services

--- src/todos/todos.service.ts
+++ src/todos/todos.service.ts
@@ -36,12 +36,23 @@ export class TodosService {
   ) {}
 
   async create(createTodoDto: CreateTodoDto): Promise<Todo> {
-    const newTodo = await this.todosRepository.create(createTodoDto);
-    return await this.todosRepository.save(newTodo);
+    try {
+      const newTodo = await this.todosRepository.create(createTodoDto);
+
+      return await this.todosRepository.save(newTodo);
+    } catch (err) {
+      throw err;
+    }
   }
 
   async findAll(): Promise<Todo[]> {
-    return await this.todosRepository.find();
+    try {
+      const resData = await this.todosRepository.find();
+
+      return resData;
+    } catch (err) {
+      throw err;
+    }
   }
 
   async findOne(id: number): Promise<Todo> {
@@ -53,18 +64,28 @@ export class TodosService {
   }
 
   async getAllTask(): Promise<Todo[]> {
-    const tasks = await this.todosRepository.find({
-      order: { priority: "ASC", createdAt: "ASC" },
-    });
-    return tasks;
+    try {
+      const tasks = await this.todosRepository.find({
+        order: { priority: "ASC", createdAt: "ASC" },
+      });
+
+      return tasks;
+    } catch (err) {
+      throw err;
+    }
   }
 
   async getFilteredTodos(priority: Priority): Promise<Todo[]> {
-    const todos = await this.todosRepository.find({
-      where: { priority },
-      order: { createdAt: "ASC" },
-    });
-    return todos;
+    try {
+      const todos = await this.todosRepository.find({
+        where: { priority },
+        order: { createdAt: "ASC" },
+      });
+
+      return todos;
+    } catch (err) {
+      throw err;
+    }
   }
 
   async getAllCompletedTask(): Promise<GetCompletedTaskResponse[]> {
@@ -80,99 +101,78 @@ export class TodosService {
     return filteredTask;
   }
 
-  async update(id: number, updateTodoDto: UpdateTodoDto): Promise<Todo> {
-    const todo = await this.findOne(id);
-    await this.todosRepository.update(todo.id, updateTodoDto);
-    return this.findOne(todo.id);
+  async updateModalTask(updatePayload: UpdateTodoDto): Promise<Todo> {
+    const { id, title, category, description } = await this.findOne(
+      updatePayload.id,
+    );
+
+    const updatedTask = {
+      title: updatePayload.title,
+      category: updatePayload.category,
+      description: updatePayload.description,
+    };
+
+    try {
+      const updatedTasks = await this.todosRepository.update(id, updatedTask);
+
+      return updatedTasks as any as Todo;
+    } catch (err) {
+      throw err;
+    }
   }
 
-  async updateModalTask(updateTaskDto: UpdateTaskModalDto): Promise<Todo> {
-    if (
-      isEmpty(updateTaskDto.category) ||
-      !Object.values(TaskType).includes(updateTaskDto.category)
-    ) {
-      throw new BadRequestException("Type is required or invalid");
-    }
-    const task = await this.findOne(updateTaskDto.id);
-    const updateTask = new UpdateTaskModal();
-    updateTask.title = updateTaskDto.title;
-    updateTask.description = updateTaskDto.description;
-    updateTask.isScheduled = isScheduled(updateTaskDto.category);
-    await this.todosRepository.update(task.id, updateTask);
-    return this.findOne(task.id);
-  }
-
-  async updateTaskByStatus(
-    id: number,
-    status: Status,
-  ): Promise<UpdateStatusResponse> {
-    if (isZeroOrNull(id)) {
-      throw new BadRequestException("ID is required");
-    }
-
-    if (isEmpty(status) || !Object.values(Status).includes(status)) {
-      throw new BadRequestException("Status is required or invalid");
-    }
+  async updateTaskByStatus(id: number, status: Status): Promise<Todo> {
     const task = await this.findOne(id);
     const completion = task.setStatus(status);
-    const updateStatus = new UpdateTaskDto();
-    updateStatus.id = id;
-    updateStatus.status = status;
-    updateStatus.completedAt = completion;
-    await this.todosRepository.update(task.id, updateStatus);
-    const data = await this.findOne(id);
-    const response = {} as UpdateStatusResponse;
-    response.id = data.id;
-    response.status = data.status;
-    return response;
+    const updatedTaskStatus = { id, status, completion } as UpdateTodoDto;
+
+    try {
+      const updates = await this.todosRepository.update(id, updatedTaskStatus);
+
+      return updates as any as Todo;
+    } catch (err) {
+      throw err;
+    }
   }
 
-  async updateTaskByPrioriy(
-    id: number,
-    priority: Priority,
-  ): Promise<UpdatePriorityResponse> {
-    if (isZeroOrNull(id)) {
-      throw new BadRequestException("ID is required");
+  async updateTaskByPrioriy(id: number, priority: Priority): Promise<Todo> {
+    await this.findOne(id);
+    const updatedTaskPriority = { id, priority } as UpdateTodoDto;
+
+    try {
+      const updates = await this.todosRepository.update(
+        id,
+        updatedTaskPriority,
+      );
+
+      return updates as any as Todo;
+    } catch (err) {
+      throw err;
     }
-    if (isEmpty(priority) || !Object.values(Priority).includes(priority)) {
-      throw new BadRequestException("Priority is required or invalid");
-    }
-    const task = await this.findOne(id);
-    const updatePriority = new UpdateTaskDto();
-    updatePriority.id = id;
-    updatePriority.priority = priority;
-    await this.todosRepository.update(task.id, updatePriority);
-    const data = await this.findOne(id);
-    const response = {} as UpdatePriorityResponse;
-    response.id = data.id;
-    response.priority = data.priority;
-    return response;
   }
 
-  async updateTaskByType(
-    id: number,
-    type: TaskType,
-  ): Promise<UpdateTaskTypeResponse> {
-    if (isZeroOrNull(id)) {
-      throw new BadRequestException("ID is required");
+  async updateTaskByType(id: number, type: TaskType): Promise<Todo> {
+    await this.findOne(id);
+    const updateTaskCategory = { id, category: type } as UpdateTodoDto;
+
+    try {
+      const updates = await this.todosRepository.update(id, updateTaskCategory);
+
+      return updates as any as Todo;
+    } catch (err) {
+      throw err;
     }
-    if (isEmpty(type) || !Object.values(TaskType).includes(type)) {
-      throw new BadRequestException("Type is required or invalid");
-    }
-    const task = await this.findOne(id);
-    const updateTask = new UpdateTaskDto();
-    updateTask.id = id;
-    updateTask.isScheduled = isScheduled(type);
-    await this.todosRepository.update(task.id, updateTask);
-    const data = await this.findOne(id);
-    const response = {} as UpdateTaskTypeResponse;
-    response.id = data.id;
-    response.isScheduled = data.isScheduled;
-    return response;
   }
 
-  async remove(id: number): Promise<void> {
+  async remove(id: number): Promise<Todo> {
     const todo = await this.findOne(id);
-    await this.todosRepository.remove(todo);
+
+    try {
+      const res = await this.todosRepository.remove(todo);
+
+      return res as Todo;
+    } catch (err) {
+      throw err;
+    }
   }
 }
