de8b3605a6d5d3db8f69b95048ff9d31237c4fa3
Dev-Api-Alqueza <mark.alqueza@mlhuillier.com>
2025-03-17 16:46:51 +0800

    [Add export bootstrap for serverless]

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import { NestFactory } from "@nestjs/core";
 import * as cookieParser from "cookie-parser";
 import helmet from "helmet";
 import { AppModule } from "./app.module";
-import "module-alias/register";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
@@ -25,3 +24,5 @@ async function bootstrap() {
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
+
+export { bootstrap };
