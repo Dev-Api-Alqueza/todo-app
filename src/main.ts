c795046ad3e9d0762d891427a0985d125448b148
Dev-Api-Alqueza <mark.alqueza@mlhuillier.com>
2025-03-17 16:18:42 +0800

    [Fix deployment]

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { NestFactory } from "@nestjs/core";
 import * as cookieParser from "cookie-parser";
 import helmet from "helmet";
 import { AppModule } from "./app.module";
+import "module-alias/register";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
